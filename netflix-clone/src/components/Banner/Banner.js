b2da9b8fd1da4c6bb0d452c295257e5338e323b4
tesheman2020 <tesheman2020@gmail.com>
2025-12-07 08:14:58 -0500

    axios requests and env

---
+++ netflix-clone/src/components/Banner/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Banner.css"
+import instance from "../../utlis/axios"
+
+const Banner = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Banner
